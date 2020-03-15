372bb7afbfc43cc43e05b1d50a7c396a5a1a6bd5
steve1 <yang.fengsen@gmail.com>
2020-03-15 14:41:29 -0600

    modal

--- pokerman/screens/PickerDropDown.js
+++ pokerman/screens/PickerDropDown.js
@@ -1,77 +1,75 @@
 import React from "react";
 import { Platform } from "react-native";
 import { Button } from "react-native-elements";
-import { Picker, View, Text } from "react-native";
+import {
+  Modal,
+  Picker,
+  Text,
+  TouchableHighlight,
+  View,
+  Alert
+} from "react-native";
 
 export default class PickerDropDown extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       showButton: true,
+      modalVisible: false,
       label: "Pick"
     };
   }
 
-  showButton = () =>
-    !this.state.showButton ? (
-      <View></View>
-    ) : (
-      <Button
-        buttonStyle={{
-          backgroundColor: "#D1D1D1"
-        }}
-        onPress={this.toggle()}
-        color="#fff000"
-        title={this.state.label}
-        onPress={this.changeOpacity}
-      />
-    );
-  changeOpacity = () => {
-    this.setState({
-      pickerOpacity: 1,
-      opacityOfOtherItems: 0,
-      showButton: false
-    });
-  };
-  showPicker = () =>
-    this.state.showButton ? (
-      <View></View>
-    ) : (
-      <Picker
-        selectedValue={this.state.selected}
-        style={{
-          height: 50,
-          alignSelf: "center",
-          marginBottom: 30,
-          width: 250
-        }}
-        onValueChange={(itemValue, itemIndex) => {
-          this.setState({
-            selected: itemValue,
-            label: itemValue
-          });
-          this.toggle();
-        }}
-      >
-        <Picker.Item label="Your Label" value="yourValue" />
-        <Picker.Item label="Your Label" value="yourValue2" />
-        <Picker.Item label="Your Label" value="yourValue3" />
-        <Picker.Item label="Your Label" value="yourValue4" />
-      </Picker>
-    );
   toggle = () => {
-    //if (Platform.OS === "ios") {
-    if (!this.state.showButton) {
-      this.setState({
-        showButton: true
-      });
-    }
+    this.setState({
+      modalVisible: !this.state.modalVisible
+    });
   };
   render() {
     return (
       <View>
-        {this.showButton()}
-        {this.showPicker()}
+        <Modal
+          animationType="slide"
+          transparent={false}
+          visible={this.state.modalVisible}
+        >
+          <View style={{ flex: 1 }}>
+            <Picker
+              selectedValue={this.state.selected}
+              style={{
+                height: 50,
+                alignSelf: "center",
+                marginBottom: 30,
+                width: 250
+              }}
+              onValueChange={(itemValue, itemIndex) => {
+                this.setState({
+                  selected: itemValue,
+                  label: itemValue
+                });
+              }}
+            >
+              <Picker.Item label="Your Label" value="yourValue" />
+              <Picker.Item label="Your Label" value="yourValue2" />
+              <Picker.Item label="Your Label" value="yourValue3" />
+              <Picker.Item label="Your Label" value="yourValue4" />
+            </Picker>
+          </View>
+          <Button
+            buttonStyle={{
+              backgroundColor: "#D1D1D1"
+            }}
+            title="Done"
+            onPress={this.toggle}
+          />
+        </Modal>
+        <Button
+          buttonStyle={{
+            backgroundColor: "#D1D1D1"
+          }}
+          title={this.state.label}
+          onPress={this.toggle}
+        />
       </View>
     );
   }
