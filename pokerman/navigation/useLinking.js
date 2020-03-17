87b53db6bf1be32ee188222a728ea072bcd0fa66
Steve Yang <steve.yang@gov.ab.ca>
2020-03-17 15:20:16 -0600

    clean up

--- pokerman/navigation/useLinking.js
+++ pokerman/navigation/useLinking.js
@@ -1,18 +1,17 @@
 import { useLinking } from "@react-navigation/native";
 import { Linking } from "expo";
-import i18n from "../i18n";
 
 export default function(containerRef) {
   return useLinking(containerRef, {
     prefixes: [Linking.makeUrl("/")],
     config: {
       Root: {
-        path: "root",
+        path: "",
         screens: {
-          Play: "play2",
-          Player: "player",
-          Seat: "seat",
-          Timer: "timer"
+          play: "play",
+          player: "player",
+          seat: "seat",
+          timer: "timer"
         }
       }
     }
