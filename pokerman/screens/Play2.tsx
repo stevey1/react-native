12ba8a08c2ea1546dd3d1b7c43c79bc103e1635d
Steve Yang <steve.yang@gov.ab.ca>
2020-03-19 14:01:59 -0600

    delete paly2

--- pokerman/screens/Play2.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import Play from "./Play";
-import { seats as defaultSeats } from "../constants/helper";
-
-const Play2 = () => (
-  <Play bigBlind={2} dealerSeatIndex={0} seats={defaultSeats}></Play>
-);
-
-export default Play2;
