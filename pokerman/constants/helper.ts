713e332257c2857a145104d5b4b86c1964c705cb
Steve Yang <steve.yang@gov.ab.ca>
2020-03-24 12:04:30 -0600

    clean up

--- pokerman/constants/helper.ts
+++ pokerman/constants/helper.ts
@@ -1,4 +1,4 @@
-import { IPlayer, ISeat, Suit, ICard, Round, PlayType } from "./DataTypes";
+import { IPlayer, Suit, ICard, Round, PlayType } from "./DataTypes";
 import i18n from "../i18n";
 const numberMap =
   i18n.locale === "en"
@@ -66,98 +66,90 @@ export const getSeatText = (seatNumber: number) =>
   ":";
 
 export const getPlayerText = (player: IPlayer) => player.name;
-const player1 = {
-  id: 1,
-  name: "Me",
-  preflopRaiseType: PlayType.M,
-  preflopCallType: PlayType.L,
-  raiseType: PlayType.M,
-  callType: PlayType.M,
-  isMe: true
-};
-const player2 = {
-  id: 2,
-  name: "P2",
-  preflopRaiseType: PlayType.M,
-  preflopCallType: PlayType.L,
-  raiseType: PlayType.M,
-  callType: PlayType.M,
-  isMe: false
-};
-const player3 = {
-  id: 3,
-  name: "P3",
-  preflopRaiseType: PlayType.M,
-  preflopCallType: PlayType.L,
-  raiseType: PlayType.M,
-  callType: PlayType.M,
-  isMe: false
-};
-const player4 = {
-  id: 4,
-  name: "P4",
-  preflopRaiseType: PlayType.M,
-  preflopCallType: PlayType.L,
-  raiseType: PlayType.M,
-  callType: PlayType.M,
-  isMe: false
-};
-const player5 = {
-  id: 5,
-  name: "P5",
-  preflopRaiseType: PlayType.M,
-  preflopCallType: PlayType.L,
-  raiseType: PlayType.M,
-  callType: PlayType.M,
-  isMe: false
-};
-const player6 = {
-  id: 6,
-  name: "P6",
-  preflopRaiseType: PlayType.M,
-  preflopCallType: PlayType.L,
-  raiseType: PlayType.M,
-  callType: PlayType.M,
-  isMe: false
-};
-const player7 = {
-  id: 7,
-  name: "P7",
-  preflopRaiseType: PlayType.M,
-  preflopCallType: PlayType.L,
-  raiseType: PlayType.M,
-  callType: PlayType.M,
-  isMe: false
-};
-const player8 = {
-  id: 8,
-  name: "P8",
-  preflopRaiseType: PlayType.M,
-  preflopCallType: PlayType.L,
-  raiseType: PlayType.M,
-  callType: PlayType.M,
-  isMe: false
-};
-const player9 = {
-  id: 9,
-  name: "P9",
-  preflopRaiseType: PlayType.M,
-  preflopCallType: PlayType.L,
-  raiseType: PlayType.M,
-  callType: PlayType.M,
-  isMe: false
-};
 
 export const AllPlayers: IPlayer[] = [
-  player1,
-  player2,
-  player3,
-  player4,
-  player5,
-  player6,
-  player7,
-  player8,
-  player9,
+  {
+    id: 1,
+    name: "Me",
+    preflopRaiseType: PlayType.M,
+    preflopCallType: PlayType.L,
+    raiseType: PlayType.M,
+    callType: PlayType.M,
+    isMe: true
+  },
+  {
+    id: 2,
+    name: "P2",
+    preflopRaiseType: PlayType.M,
+    preflopCallType: PlayType.L,
+    raiseType: PlayType.M,
+    callType: PlayType.M,
+    isMe: false
+  },
+  {
+    id: 3,
+    name: "P3",
+    preflopRaiseType: PlayType.M,
+    preflopCallType: PlayType.L,
+    raiseType: PlayType.M,
+    callType: PlayType.M,
+    isMe: false
+  },
+  {
+    id: 4,
+    name: "P4",
+    preflopRaiseType: PlayType.M,
+    preflopCallType: PlayType.L,
+    raiseType: PlayType.M,
+    callType: PlayType.M,
+    isMe: false
+  },
+  {
+    id: 5,
+    name: "P5",
+    preflopRaiseType: PlayType.M,
+    preflopCallType: PlayType.L,
+    raiseType: PlayType.M,
+    callType: PlayType.M,
+    isMe: false
+  },
+  {
+    id: 6,
+    name: "P6",
+    preflopRaiseType: PlayType.M,
+    preflopCallType: PlayType.L,
+    raiseType: PlayType.M,
+    callType: PlayType.M,
+    isMe: false
+  },
+  {
+    id: 7,
+    name: "P7",
+    preflopRaiseType: PlayType.M,
+    preflopCallType: PlayType.L,
+    raiseType: PlayType.M,
+    callType: PlayType.M,
+    isMe: false
+  },
+  {
+    id: 8,
+    name: "P8",
+    preflopRaiseType: PlayType.M,
+    preflopCallType: PlayType.L,
+    raiseType: PlayType.M,
+    callType: PlayType.M,
+    isMe: false
+  },
+  {
+    id: 9,
+    name: "P9",
+    preflopRaiseType: PlayType.M,
+    preflopCallType: PlayType.L,
+    raiseType: PlayType.M,
+    callType: PlayType.M,
+    isMe: false
+  },
+
   {
     id: 10,
     name: "P10",
@@ -196,33 +188,6 @@ export const AllPlayers: IPlayer[] = [
   }
 ];
 
-export const seats: ISeat[] = [
-  { player: player1, seatNumber: 0, betOrder: 0 },
-  { player: player2, seatNumber: 1, betOrder: 1 },
-  { player: player3, seatNumber: 2, betOrder: 2 },
-  { player: player4, seatNumber: 3, betOrder: 3 },
-  { player: player5, seatNumber: 4, betOrder: 4 },
-  { player: player6, seatNumber: 5, betOrder: 5 },
-  { player: player7, seatNumber: 6, betOrder: 6 }
-];
-export const runRules = (
-  round: Round,
-  myHand: ICard[],
-  board: ICard,
-  mySeatNumber: number,
-  dealerSeatNumber: number,
-  totalPlayers: number
-) => {
-  if (round === Round.Preflop) {
-  }
-};
-export const runPreRules = (
-  myHand: ICard[],
-  myPosition: number,
-  dealerSeatNumber: number,
-  totalPlayers: number
-) => {};
-
 const ordinal_of = n =>
   n.toString() +
   (["st", "nd", "rd"][(((((n < 0 ? -n : n) + 90) % 100) - 10) % 10) - 1] ||
@@ -231,65 +196,6 @@ const ordinal_of = n =>
 export const getNumberOrdinal = n =>
   i18n.locale === "en" ? ordinal_of(n) : getNumberText(n);
 
-const rules = {
-  myHand: {
-    AA:
-      "Prefere to reduce to 2- callers; Must Raise 3XPot, under the gun: check raise; canot raise: prepare to fold after flop",
-    KK:
-      "Prefere to reduce to 2- callers; Must Raise 3XPot - Watch AA, under the gun: check raise; canot raise: prepare to fold after flop",
-    QQJJ:
-      "Re-raise to 55-75 dollars, people call you must have A*, prepare to fold flop A",
-    AJ: "Raise preflop, 45$",
-    AT: "Raise preflop; 45$",
-    A9_:
-      "Good caller hand vs pocket pair vs JJ -> Raise preflop, small raise without position. 45$",
-    KJ: "raise preflop, not call over 20$",
-    QJ: "Raise preflop, small raise without position. not call 20$",
-    TT99: "Rasie befor flop, worth 45$",
-    P88_: "Small rasie before flop with position; worth 35$"
-  },
-  preFlop: {
-    raise: "A*, KJ* or pocket? 3 A on play. Is my kicker big", //Who will raise or call, 50%->A*, 30%->K*, 20%->poket pair
-    raise_bigger: "Poket pair? TT-QQ? 3 A on play. Is my kicker big", //Who will raise or call, 50%->A*, 30%->K*, 20%->poket pair
-    Raise2: "Raise 2 - TT/JJ/QQ? or KK/AA?",
-    Raise3: "Raise 3 - KK/AA?",
-    checkRaise: "Check Raise - Must be AA, at least KK?" //Who will raise or call, 50%->A*, 30%->K*, 20%->poket pair
-  },
-  flop: {
-    theirHand: "More like: A*, K* or pocket",
-    reraise_dry_board: "two pair or set?",
-    reraise_wet_board: "straight?",
-    reraise_water_board: "two pair or set?",
-    myAction: {
-      potBet: "Pot bet ",
-      AllIn: "I all in. With J+ pair A kicker/overpair/2 pairs, "
-    }
-  },
-  turn: {
-    myAction: {
-      potBet: "Pot bet ",
-      AllIn: "I all in. With J+ pair A kicker/overpair/2 pairs, "
-    }
-  },
-  river: {
-    myAction: {
-      notCallAllIn: "Don't call all in"
-    }
-  },
-  board: {
-    fullHouse: "Full House on board-prepare to fold, don't draw(dead)",
-    flushOnBoard: "Flush on board-prepare to fold, don't draw(dead)",
-    strarightOnBoard: "Straight on board-prepare to fold",
-    eazyStrarightOnBoard:
-      "4 cards Straight on board-prepare to fold, don't draw(dead)"
-  },
-  raise: {
-    checkRaise: "Check Rasise"
-  },
-  notCallAllIn:
-    "Don't call all in, unless he has short stack(losing), impatient, or I have the nuts!!!",
-  allIn: "I all in. With J+ pair A kicker/overpair/2 pairs, "
-};
 export const players = [
   {
     __typename: "player",
