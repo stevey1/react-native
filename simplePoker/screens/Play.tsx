fd8b7ede3defee9b034773979fa91262c463a4f5
Steve Yang <steve.yang@gov.ab.ca>
2020-03-31 15:55:17 -0600

    add some images

--- simplePoker/screens/Play.tsx
+++ simplePoker/screens/Play.tsx
@@ -1,22 +1,34 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Text, View } from "react-native";
 import { ScrollView } from "react-native-gesture-handler";
-import { Button } from "react-native-elements";
+import { Button, Image } from "react-native-elements";
 import i18n from "../i18n";
 import { getNumberText, getSuitText } from "../constants/helper";
 import { getMyHandPreflop, checkBoard, checkMyHand } from "../constants/master";
 import styles from "./styles";
-
 import { ICard } from "../constants/DataTypes";
+import { Dimensions } from "react-native";
 export default function Play() {
   const [SuitOrNumber, setSuitOrNumber] = useState(1);
+  const [Pictures, setPictures] = useState([0, 1, 2]);
   const [CardIndex, setCardIndex] = useState(0);
   const [MyHand, setMyHand] = useState([]);
   const [Board, setBoard] = useState([]);
+  const windowHeight = Dimensions.get("window").height;
+
+  useEffect(() => {
+    setInterval(() => {
+      const pic = [
+        Math.round(Math.random() * 41) % 21,
+        Math.round(Math.random() * 35) % 21,
+        Math.round(Math.random() * 36) % 21
+      ];
+      setPictures(pic);
+    }, 20000);
+  });
 
   const handleCard = back => {
-    if (back < 0) back = 14 + back;
-    console.log(back);
+    if (back < 0) back = 13 + back;
     if (CardIndex < 2) {
       const cardIndex = CardIndex;
       const myHand = [...MyHand];
@@ -58,6 +70,7 @@ export default function Play() {
       .filter(x => x)
       .sort((c1, c2) => c1.cardNumber - c2.cardNumber);
     let tips = [];
+    let results = [];
 
     if (board.length < 3) {
       if (myHand.length == 2) {
@@ -75,24 +88,28 @@ export default function Play() {
           );
       }
     } else {
-      tips.push(
-        <View key="b" style={{ flexDirection: "row" }}>
-          <Text key="b1" style={styles.text}>
-            {i18n.t("play.board")}:
-          </Text>
-          <View>{formatResult(checkBoard(board))}</View>
-        </View>
-      );
       if (myHand.length == 2) {
+        results = checkMyHand(board, myHand);
+        if (results.length > 0)
+          tips.push(
+            <View key="i" style={{ flexDirection: "row" }}>
+              <Text key="i1" style={styles.text}>
+                {i18n.t("play.iHave")}:{" "}
+              </Text>
+              <View>{formatResult(checkMyHand(board, myHand))}</View>
+            </View>
+          );
+      }
+      results = checkBoard(board);
+      if (results.length > 0)
         tips.push(
-          <View key="i" style={{ flexDirection: "row" }}>
-            <Text key="i1" style={styles.text}>
-              {i18n.t("play.iHave")}:{" "}
+          <View key="b" style={{ flexDirection: "row" }}>
+            <Text key="b1" style={styles.text}>
+              {i18n.t("play.board")}:
             </Text>
-            <View>{formatResult(checkMyHand(board, myHand))}</View>
+            <View>{formatResult(checkBoard(board))}</View>
           </View>
         );
-      }
     }
     return tips.length > 0 ? <View>{tips}</View> : <View></View>;
   };
@@ -106,63 +123,101 @@ export default function Play() {
   return (
     <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
       <View style={styles.container}>
-        <View style={{ flexDirection: "row", justifyContent: "space-between" }}>
-          <View style={{ flexDirection: "row" }}>
-            <Button
-              key="cardIndex"
-              buttonStyle={{ backgroundColor: "#D1D1D1" }}
-              style={{ marginRight: 3 }}
-              title={getNumberText(CardIndex + 1)}
-              titleStyle={{ color: "#000000" }}
-              onPress={() => setCardIndex((CardIndex + 1) % 7)}
-            />
-            <Button
-              key="suitOrNumber"
-              buttonStyle={{ backgroundColor: "#D1D1D1" }}
-              style={{ marginRight: 3 }}
-              title={i18n.t("suitOrNumber." + SuitOrNumber.toString())}
-              titleStyle={{ color: "#000000" }}
-              onPress={() => setSuitOrNumber((SuitOrNumber + 1) % 2)}
-            />
-          </View>
-          <Text>
-            {displayCards(MyHand)} - {displayCards(Board)}
-          </Text>
-          <View style={{ flexDirection: "row" }}>
-            <Button
-              key="forward"
-              buttonStyle={{ backgroundColor: "#D1D1D1" }}
-              title={i18n.t("button.forward")}
-              titleStyle={{ color: "#000000" }}
-              onPress={() => handleCard(1)}
-              style={{ marginRight: 3 }}
-            />
-            <Button
-              key="backward"
-              buttonStyle={{ backgroundColor: "#D1D1D1" }}
-              title={i18n.t("button.backward")}
-              titleStyle={{ color: "#000000" }}
-              onPress={() => handleCard(-3)}
-              style={{ marginRight: 3 }}
-            />
-          </View>
+        <View>
+          <Image
+            key="1"
+            style={{
+              height: windowHeight / 3,
+              resizeMode: "stretch"
+            }}
+            source={require("../assets/images/images (" +
+              Pictures[0] +
+              ").jfif")}
+          />
+          <Image
+            key="3"
+            style={{
+              height: windowHeight / 3,
+              resizeMode: "stretch"
+            }}
+            source={require("../assets/images/images (" +
+              Pictures[1] +
+              ").jfif")}
+          />
+          <Image
+            key="5"
+            style={{
+              height: windowHeight / 3,
+              resizeMode: "stretch"
+            }}
+            source={require("../assets/images/images (" +
+              Pictures[2] +
+              ").jfif")}
+          />
         </View>
-        <View style={{ flexDirection: "row", justifyContent: "space-between" }}>
-          {showTips()}
-          <View style={{ flexDirection: "row" }}>
-            <Button
-              key="new"
-              buttonStyle={{ backgroundColor: "#D1D1D1" }}
-              title={i18n.t("button.new")}
-              titleStyle={{ color: "#000000" }}
-              onPress={() => {
-                setMyHand([]);
-                setBoard([]);
-                setSuitOrNumber(1);
-                setCardIndex(0);
-              }}
-              style={{ marginRight: 3 }}
-            />
+        <View>
+          <View
+            style={{ flexDirection: "row", justifyContent: "space-between" }}
+          >
+            {showTips()}
+            <View style={{ flexDirection: "row" }}>
+              <Button
+                key="new"
+                buttonStyle={{ backgroundColor: "#D1D1D1" }}
+                title={i18n.t("button.new")}
+                titleStyle={{ color: "#000000" }}
+                onPress={() => {
+                  setMyHand([]);
+                  setBoard([]);
+                  setSuitOrNumber(1);
+                  setCardIndex(0);
+                }}
+                style={{ marginRight: 3 }}
+              />
+            </View>
+          </View>
+          <View
+            style={{ flexDirection: "row", justifyContent: "space-between" }}
+          >
+            <View style={{ flexDirection: "row" }}>
+              <Button
+                key="cardIndex"
+                buttonStyle={{ backgroundColor: "#D1D1D1" }}
+                style={{ marginRight: 3 }}
+                title={getNumberText(CardIndex + 1)}
+                titleStyle={{ color: "#000000" }}
+                onPress={() => setCardIndex((CardIndex + 1) % 7)}
+              />
+              <Button
+                key="suitOrNumber"
+                buttonStyle={{ backgroundColor: "#D1D1D1" }}
+                style={{ marginRight: 3 }}
+                title={i18n.t("suitOrNumber." + SuitOrNumber.toString())}
+                titleStyle={{ color: "#000000" }}
+                onPress={() => setSuitOrNumber((SuitOrNumber + 1) % 2)}
+              />
+            </View>
+            <Text>
+              {displayCards(MyHand)} - {displayCards(Board)}
+            </Text>
+            <View style={{ flexDirection: "row" }}>
+              <Button
+                key="forward"
+                buttonStyle={{ backgroundColor: "#D1D1D1" }}
+                title={i18n.t("button.forward")}
+                titleStyle={{ color: "#000000" }}
+                onPress={() => handleCard(1)}
+                style={{ marginRight: 3 }}
+              />
+              <Button
+                key="backward"
+                buttonStyle={{ backgroundColor: "#D1D1D1" }}
+                title={i18n.t("button.backward")}
+                titleStyle={{ color: "#000000" }}
+                onPress={() => handleCard(-3)}
+                style={{ marginRight: 3 }}
+              />
+            </View>
           </View>
         </View>
       </View>
